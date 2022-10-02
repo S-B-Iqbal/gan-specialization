9f99e78c8c3c88529db7ff520f2f1c7f93d62274
S-B-Iqbal <buland.shahrukh@gmail.com>
2022-10-02 14:27:07 +0200

    Create controllable_gan.py

---
+++ build-basic-gan/scripts/controllable_gan.py
@@ -0,0 +1,260 @@
+"""
+Controllable GAN along with entanglement and regularization
+"""
+
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+import torch
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torchvision.datasets import CelebA
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0)
+
+def show_tensor_images(image_tensor, num_images=16, size=(3, 64, 64), nrow=3):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+    
+# Gen
+class Generator(nn.Module):
+    '''
+    Generator Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (CelebA is rgb, so 3 is our default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, z_dim=10, im_chan=3, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 8),
+            self.make_gen_block(hidden_dim * 8, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN;
+        a transposed convolution, a batchnorm (except in the final layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        x = noise.view(len(noise), self.z_dim, 1, 1)
+        return self.gen(x)
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples in the batch, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
+
+# Classifier
+class Classifier(nn.Module):
+    '''
+    Classifier Class
+    Values:
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (CelebA is rgb, so 3 is our default)
+        n_classes: the total number of classes in the dataset, an integer scalar
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, im_chan=3, n_classes=2, hidden_dim=64):
+        super(Classifier, self).__init__()
+        self.classifier = nn.Sequential(
+            self.make_classifier_block(im_chan, hidden_dim),
+            self.make_classifier_block(hidden_dim, hidden_dim * 2),
+            self.make_classifier_block(hidden_dim * 2, hidden_dim * 4, stride=3),
+            self.make_classifier_block(hidden_dim * 4, n_classes, final_layer=True),
+        )
+
+    def make_classifier_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a classifier block; 
+        a convolution, a batchnorm (except in the final layer), and an activation (except in the final layer).
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the classifier: Given an image tensor, 
+        returns an n_classes-dimension tensor representing fake/real.
+        Parameters:
+            image: a flattened image tensor with im_chan channels
+        '''
+        class_pred = self.classifier(image)
+        return class_pred.view(len(class_pred), -1)
+      
+# Training
+
+z_dim = 64
+batch_size = 128
+device = 'cuda'
+
+## Loading PreTrained classifier and Generator
+gen = Generator(z_dim).to(device)
+gen_dict = torch.load("pretrained_celeba.pth", map_location=torch.device(device))["gen"]
+gen.load_state_dict(gen_dict)
+gen.eval()
+
+n_classes = 40
+classifier = Classifier(n_classes=n_classes).to(device)
+class_dict = torch.load("pretrained_classifier.pth", map_location=torch.device(device))["classifier"]
+classifier.load_state_dict(class_dict)
+classifier.eval()
+print("Loaded the models!")
+
+opt = torch.optim.Adam(classifier.parameters(), lr=0.01)
+
+### Updated Noise from classifier: new_moise = old_noise + (∇ old * weight)
+### NOTE: Gradient Ascent rather than descent!
+
+def calculate_updated_noise(noise, weight):
+    '''
+    Function to return noise vectors updated with stochastic gradient ascent.
+    Parameters:
+        noise: the current noise vectors. You have already called the backwards function on the target class
+          so you can access the gradient of the output class with respect to the noise by using noise.grad
+        weight: the scalar amount by which you should weight the noise gradient
+    '''
+    new_noise = noise + (noise.grad*weight)
+    return new_noise
+
+# Generation
+
+# First generate a bunch of images with the generator
+n_images = 8
+fake_image_history = []
+grad_steps = 10 # Number of gradient steps to take
+skip = 2 # Number of gradient steps to skip in the visualization
+
+feature_names = ["5oClockShadow", "ArchedEyebrows", "Attractive", "BagsUnderEyes", "Bald", "Bangs",
+"BigLips", "BigNose", "BlackHair", "BlondHair", "Blurry", "BrownHair", "BushyEyebrows", "Chubby",
+"DoubleChin", "Eyeglasses", "Goatee", "GrayHair", "HeavyMakeup", "HighCheekbones", "Male", 
+"MouthSlightlyOpen", "Mustache", "NarrowEyes", "NoBeard", "OvalFace", "PaleSkin", "PointyNose", 
+"RecedingHairline", "RosyCheeks", "Sideburn", "Smiling", "StraightHair", "WavyHair", "WearingEarrings", 
+"WearingHat", "WearingLipstick", "WearingNecklace", "WearingNecktie", "Young"]
+
+### Change me! ###
+target_indices = feature_names.index("Attractive") # Feel free to change this value to any string from feature_names!
+
+noise = get_noise(n_images, z_dim).to(device).requires_grad_()
+for i in range(grad_steps):
+    opt.zero_grad()
+    fake = gen(noise)
+    fake_image_history += [fake]
+    fake_classes_score = classifier(fake)[:, target_indices].mean()
+    fake_classes_score.backward()
+    noise.data = calculate_updated_noise(noise, 1 / grad_steps)
+
+plt.rcParams['figure.figsize'] = [n_images * 2, grad_steps * 2]
+show_tensor_images(torch.cat(fake_image_history[::skip], dim=2), num_images=n_images, nrow=n_images)
+
+# Entanglement and Regularization
+def get_score(current_classifications, original_classifications, target_indices, other_indices, penalty_weight):
+    '''
+    Function to return the score of the current classifications, penalizing changes
+    to other classes with an L2 norm.
+    Parameters:
+        current_classifications: the classifications associated with the current noise
+        original_classifications: the classifications associated with the original noise
+        target_indices: the index of the target class
+        other_indices: the indices of the other classes
+        penalty_weight: the amount that the penalty should be weighted in the overall score
+    '''
+    # Steps: 1) Calculate the change between the original and current classifications (as a tensor)
+    #           by indexing into the other_indices you're trying to preserve, like in x[:, features].
+    #        2) Calculate the norm (magnitude) of changes per example.
+    #        3) Multiply the mean of the example norms by the penalty weight. 
+    #           This will be your other_class_penalty.
+    #           Make sure to negate the value since it's a penalty!
+    #        4) Take the mean of the current classifications for the target feature over all the examples.
+    #           This mean will be your target_score.
+    # Calculate the error on other_indices
+    other_distances = current_classifications[:,other_indices] - original_classifications[:,other_indices]
+    # Calculate the norm (magnitude) of changes per example and multiply by penalty weight
+    other_class_penalty = -(torch.norm(other_distances, dim=1).mean() * penalty_weight)
+    # Take the mean of the current classifications for the target feature
+    target_score = current_classifications[:,target_indices].mean()
+    return target_score + other_class_penalty
+  
+fake_image_history = []
+### Change me! ###
+target_indices = feature_names.index("Smiling") # Feel free to change this value to any string from feature_names from earlier!
+other_indices = [cur_idx != target_indices for cur_idx, _ in enumerate(feature_names)]
+noise = get_noise(n_images, z_dim).to(device).requires_grad_()
+original_classifications = classifier(gen(noise)).detach()
+for i in range(grad_steps):
+    opt.zero_grad()
+    fake = gen(noise)
+    fake_image_history += [fake]
+    fake_score = get_score(
+        classifier(fake), 
+        original_classifications,
+        target_indices,
+        other_indices,
+        penalty_weight=0.1
+    )
+    fake_score.backward()
+    noise.data = calculate_updated_noise(noise, 1 / grad_steps)
+
+plt.rcParams['figure.figsize'] = [n_images * 2, grad_steps * 2]
+show_tensor_images(torch.cat(fake_image_history[::skip], dim=2), num_images=n_images, nrow=n_images)
